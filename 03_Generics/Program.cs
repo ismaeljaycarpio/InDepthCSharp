5cf252ec6eb8a5fd9a39fdc8a66f4bdad257ee4c
ismael <ismaeljaycarpio1992@gmail.com>
2018-05-16 11:20:10 +0800

    added chapter 3

---
+++ 03_Generics/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = @"Do you like green eggs and ham?
+                            I do not like them, Sam-I-am.
+                            I do not like green eggs and ham.";
+
+            Dictionary<string, int> list = CountWords(text);
+            foreach(KeyValuePair<string,int> entry in list)
+            {
+                string word = entry.Key;
+                int frequency = entry.Value;
+                Console.WriteLine("{0} : {1}", word, frequency);
+            }
+
+            Console.ReadLine();
+        }
+
+        static Dictionary<string, int> CountWords(string text)
+        {
+            Dictionary<string, int> frequencies = new Dictionary<string, int>();
+
+            string[] words = Regex.Split(text, @"\W+");
+
+            foreach(string word in words)
+            {
+                if(frequencies.ContainsKey(word))
+                {
+                    frequencies[word]++;
+                }
+                else
+                {
+                    frequencies[word] = 1;
+                }
+            }
+
+            return frequencies;
+        }
+    }
+}
---
+++ StringManipulation/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string originalStr = "Ismael Jay Carpio";
+
+            //foreach (char c in originalStr)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            //char[] delim = {' '};
+            //string[] strArr = originalStr.Split(delim);
+
+            //foreach(string s in strArr)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //Console.WriteLine(originalStr.Substring(1));
+            //Console.WriteLine(originalStr.Length);
+
+            string strInput = @"Username:1234\Password:abcd\Name:Ismael";
+            strInput = @"Password:abcd\Name:Ismael\Username:1234";
+            Console.WriteLine(strInput);
+            char[] delim = {'\\'};
+            string[] strArr = strInput.Split(delim);
+
+            string strUsername = "";
+            string strPassword = "";
+            string strName = "";
+
+            foreach(string s in strArr)
+            {
+                //username
+                if(s.Contains("Username"))
+                {
+                    strUsername = s.Substring(s.IndexOf(':'));
+                }
+
+                //username
+                if (s.Contains("Password"))
+                {
+                    strPassword = s.Substring(s.IndexOf(':'));
+                }
+
+                //username
+                if (s.Contains("Name"))
+                {
+                    strName = s.Substring(s.IndexOf(':'));
+                }
+            }
+
+            Console.WriteLine("Username is {0} \nPassword is {1} \nName is {2}",
+                strUsername, strPassword, strName);
+            Console.ReadLine();
+        }
+    }
+}

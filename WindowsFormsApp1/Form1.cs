d8a772b498ed34b4809a2082fc9506e5ebdeb648
ismael <ismaeljaycarpio1992@gmail.com>
2018-05-18 11:59:36 +0800

    added chapter 3

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //[System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        //private static extern bool AllocConsole();
+
+        public Form1()
+        {
+            //AllocConsole();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strInput = Console.ReadLine();
+            label1.Text = strInput;
+        }
+    }
+}
